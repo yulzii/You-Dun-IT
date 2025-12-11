e760a42627084c083e2b95e21cace7ceb2b63132
Yesui Ulziibayar <yulzii789@gmail.com>
2025-12-11 09:02:06 -0600

    new images and more screen switching

--- credits.md
+++ credits.md
@@ -1,3 +1,7 @@
 #Credits
 
 1. Art by no2games, CC0, https://opengameart.org/content/office-space-tileset
+2. Art by jmel7371, CC0, https://opengameart.org/content/desktop-computer
+3. Art by kickstand284, CC0, https://opengameart.org/content/papernote-game-icon-32x32
+4. Art by Sevarihk, CC-BY 4.0, https://opengameart.org/content/pixel-art-documents
+5. Art by Sanglorian, CC-BY-SA 3.0, https://opengameart.org/content/charactertrainer-sprites-64px
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const startScreen = document.getElementById("start");
 const roomScreen = document.getElementById("room");
 const startButton = document.getElementById("startButton");
+const computerScreen = document.getElementById("computer_screen");
+const computer = document.getElementById("computer");
 function showScreen(screen) {
   document
     .querySelectorAll(".screen")
@@ -10,5 +12,8 @@ function showScreen(screen) {
 document.addEventListener("DOMContentLoaded", () => {
   startButton.addEventListener("click", () => {
     showScreen(roomScreen);
+  });
+  computer.addEventListener("click", () => {
+    showScreen(computerScreen);
   });
 });
